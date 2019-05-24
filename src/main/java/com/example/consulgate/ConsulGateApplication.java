c4cbd55accf3b283309b1dfc90dae01b7bf24246
xb <xiaobin@163.com>
2019-05-24 14:50:17 +0800

    初始化

---
+++ src/main/java/com/example/consulgate/ConsulGateApplication.java
@@ -0,0 +1,13 @@
+package com.example.consulgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulGateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/consulgate/ConsulGateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.consulgate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsulGateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
